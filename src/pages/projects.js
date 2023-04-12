21864388e6a38ee5fdc3370bc1391f25fef3a903
akifz21 <akifz2199@gmail.com>
2023-04-13 02:15:55 +0300

    update

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,35 @@
 import Card from '@/components/Card'
 import Layout from '@/components/Layout'
 import Head from 'next/head'
+import { use, useEffect, useState } from 'react'
 
 
-export default function projects({ repos }) {
+export default function projects({ GITHUB_AUTH_TOKEN }) {
+    const headers = {
+        'Authorization': `Bearer ${GITHUB_AUTH_TOKEN}`
+    }
+    const [repos, setRepos] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    const getRepos = async () => {
+        setIsLoading(true)
+        try {
+            const res = await fetch(`https://api.github.com/users/akifz21/repos?sort=created`, { headers })
+            const data = await res.json()
+            const repos = data.sort((a, b) => b.watchers - a.watchers)
+            setRepos(repos)
+            setIsLoading(false)
+        } catch (error) {
+            setIsLoading(false)
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        console.log(process.env.GITHUB_AUTH_TOKEN)
+        getRepos()
+    }, [])
+
     return (
         <Layout>
             <Head>
@@ -14,31 +40,31 @@ export default function projects({ repos }) {
                     <h1 className="md:text-4xl text-2xl font-bold">Projects 📚</h1>
                 </div>
                 {
-                    repos.map((repo) => (
-                        <Card key={repo.id} name={repo.name} watchers={repo.watchers} description={repo.description} url={repo.html_url} />
-                    ))
+                    isLoading ? <div
+                        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                        role="status">
+                        <span
+                            className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                        >Loading...</span
+                        >
+                    </div> :
+                        repos.map((repo) => (
+                            <Card key={repo.id} name={repo.name} watchers={repo.watchers} description={repo.description} url={repo.html_url} />
+                        ))
                 }
+
             </div>
         </Layout>
 
     )
 }
-export async function getServerSideProps(context) {
-    const { GITHUB_AUTH_TOKEN } = process.env;
 
-    const headers = {
-        'Authorization': `Bearer ${GITHUB_AUTH_TOKEN}`
-    }
-    try {
-        const res = await fetch(`https://api.github.com/users/akifz21/repos?sort=created`, { headers })
-        const data = await res.json()
-        const repos = data.sort((a, b) => b.watchers - a.watchers)
-        return { props: { repos } }
-    } catch (error) {
-        console.error(error);
-        return { props: {} };
+export async function getStaticProps() {
+    const { GITHUB_AUTH_TOKEN } = process.env
+
+    return {
+        props: {
+            GITHUB_AUTH_TOKEN,
+        },
     }
 }
-
-
-
